75724cb8d39bfc10cd8ed2d2244871c8051d0844
Daniel Nguyen <Koiya@users.noreply.github.com>
2023-11-19 15:47:03 -0600

    backend express js setup

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-import sqlite3 from 'sqlite3';
+// Create express app
+const express = require("express");
+const app = express()
+// Server port
+const PORT = 8000
 
-const sql = sqlite3.verbose();
-const db = new sql.Database('database file goes here', (err) => {
+//SQLite DB
+const sql = require('sqlite3').verbose();
+const db = new sql.Database('database file goes here', sql.OPEN_READWRITE, (err) => {
     //Connection errors check
     if (err) {
-        return console.error(err.message);
+        return console.error(err);
     }
     console.log('Connected to database.');
-    });
-    //Close database connection
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Closed connection.');
-    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",PORT))
+});
+
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+// Insert here other API endpoints
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
+
