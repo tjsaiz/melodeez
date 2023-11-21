7e741c027b448bdd60d270b8f249b94aadff8a78
Daniel Nguyen <Koiya@users.noreply.github.com>
2023-11-21 16:39:17 -0600

    backend updates

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,54 @@ app.listen(PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",PORT))
 });
 
-// Root endpoint
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
 
-// Insert here other API endpoints
+//Get musics/songs list
+app.get("/musicList", (req,res) => {
+    const getListQuery = `SELECT * FROM ...`
+    db.all (getListQuery, (err,rows)=>{
+        if(err){
+            return console.log(err.message);
+        }
+
+    })
+});
+
+//Update music list
+app.patch("/update", (req,res) => {
+    const updateQuery = `UPDATE ... FROM ... `
+    db.run(updateQuery, (err,res)=>{
+        if(err){
+            return console.log(err.message);
+        }
+
+    });
+});
+
+//Insert song
+app.post("/add", (req,res)=>{
+    const insertQuery = `INSERT ... FROM ...`
+    db.run(insertQuery , (err,res)=>{
+        if(err){
+            return console.log(err.message);
+        }
+
+    });
+});
+
+//Delete song
+app.delete("/remove", (req,res)=>{
+    const deleteQuery = `DELETE ... FROM ...`
+    db.run(deleteQuery , (err,res)=>{
+        if(err){
+            return console.log(err.message);
+        }
+
+    });
+});
+
 
 // Default response for any other request
 app.use(function(req, res){
